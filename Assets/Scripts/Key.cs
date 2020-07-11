ab37e0186d92c5af03df5ce7096c145812a6e87d
yy404 <yuhui.yaocn@gmail.com>
2020-07-11 15:44:36 +0100

    Minimum base

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public string keyValue;
+    private TaskManager taskManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taskManager = GameObject.FindWithTag("TaskManager").GetComponent<TaskManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        // Debug.Log(keyValue);
+        taskManager.AddToPlayerString(keyValue);
+    }
+}
---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    public string taskString;
+    private string playerString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerString = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerString == taskString)
+        {
+            Debug.Log("Win!");
+            playerString = "";
+        }
+    }
+
+    public void AddToPlayerString(string thisString)
+    {
+        playerString += thisString;
+        Debug.Log(playerString);
+    }
+}
