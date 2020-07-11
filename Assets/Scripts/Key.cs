dafd6ac387cd362066cbdd3be9ea443e47245359
yy404 <yuhui.yaocn@gmail.com>
2020-07-11 19:08:12 +0100

    Added force and collisions after click

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public string keyValue;
+    public float clickForce = 1000;
+
     private TaskManager taskManager;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         taskManager = GameObject.FindWithTag("TaskManager").GetComponent<TaskManager>();
+        rb = GetComponent<Rigidbody2D>();
+
+        clickForce = 1000;
     }
 
     // Update is called once per frame
@@ -34,5 +40,10 @@ public class Key : MonoBehaviour
         {
             taskManager.AddToPlayerString(keyValue);
         }
+
+        Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        clickPosition.z = 0;
+        Vector3 moveDirection = transform.position - clickPosition;
+        rb.AddForce(moveDirection * clickForce);
     }
 }
